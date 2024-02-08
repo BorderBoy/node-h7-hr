138f257d1b8d1fcb87a26bbd6c5abe63aebc43a5
Jonas Goos <jonasgoos@goos.de>
2024-02-08 10:52:41 +0100

    Add time information to measurements

--- index.js
+++ index.js
@@ -31,10 +31,18 @@ noble.on('discover', function(peripheral) {
     peripheral.discoverSomeServicesAndCharacteristics(serviceUUID, characteristicUUID, function(error, services, characteristics){
       characteristics[0].notify(true, function(error){
         characteristics[0].on('data', function(data, isNotification){
+          const date = new Date();
           // Upon receiving data, output the BPM
           // The actual BPM data is stored in the 2nd bit in data (at array index 1)
           // Thanks Steve Daniel: http://www.raywenderlich.com/52080/introduction-core-bluetooth-building-heart-rate-monitor
           // Measurement docs here: https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+          options = {
+            "hour": "2-digit",
+            "minute": "2-digit",
+            "second": "2-digit",
+            "fractionalSecondDigits": 3
+          }
+          process.stdout.write("[" + date.toLocaleTimeString('de-DE', options)+ "] ")
           process.stdout.write('BPM: ' + data[1] + ", RR: ");
           for (var i = 0; i < (data.length-2)/2; i++) {
             process.stdout.write((data.readUint16LE(2+2*i)/1024).toPrecision(2) + " ");
