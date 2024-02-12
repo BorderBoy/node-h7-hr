83f68bbfa39845500f769ff0952e55d807776173
Jonas Goos <jonasgoos@goos.de>
2024-02-12 13:46:19 +0100

    Add ZMQ server to initiate recording

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-var noble = require('@abandonware/noble');
+const noble = require('@abandonware/noble');
+const zmq = require('zeromq');
+const fs = require('node:fs');
+const { log } = require('node:console');
+
+var record = false;
+var file;
+var startTimestamp;
+
+async function runServer() {
+  const sock = new zmq.Reply();
+
+  await sock.bind('tcp://*:5555');
+
+  for await (const [msg] of sock) {
+    if (msg.toString().startsWith("start")) {
+      record = true
+      startTimestamp = new Date().getTime();
+      var filename = msg.toString().split(":")[1];
+      file = fs.createWriteStream(filename + '.csv', {flags: 'a'});
+      log("Start recording...");
+      await sock.send('OK');
+    } else if (msg.toString() == "stop") {
+      record = false
+      file.close();
+      log("Stop recording...");
+      await sock.send('OK');
+    } else {
+      await sock.send('ERROR');
+      log("Unknown command");
+    }
+  }
+}
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -31,11 +63,10 @@ noble.on('discover', function(peripheral) {
     peripheral.discoverSomeServicesAndCharacteristics(serviceUUID, characteristicUUID, function(error, services, characteristics){
       characteristics[0].notify(true, function(error){
         characteristics[0].on('data', function(data, isNotification){
+          var line = ""
+
           const date = new Date();
-          // Upon receiving data, output the BPM
-          // The actual BPM data is stored in the 2nd bit in data (at array index 1)
-          // Thanks Steve Daniel: http://www.raywenderlich.com/52080/introduction-core-bluetooth-building-heart-rate-monitor
-          // Measurement docs here: https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+          
           options = {
             "hour": "2-digit",
             "minute": "2-digit",
@@ -43,14 +74,23 @@ noble.on('discover', function(peripheral) {
             "fractionalSecondDigits": 3
           }
           process.stdout.write("[" + date.toLocaleTimeString('de-DE', options)+ "] ")
+          line += new Date().getTime() - startTimestamp + ",";
           process.stdout.write('BPM: ' + data[1] + ", RR: ");
+          line += data[1] + ",";
           for (var i = 0; i < (data.length-2)/2; i++) {
             process.stdout.write((data.readUint16LE(2+2*i)/1024).toPrecision(2) + " ");
+            line += (data.readUint16LE(2+2*i)/1024).toPrecision(2) + ",";
           }
           console.log("");
-          // console.log(data[0].toString(2));
+          line += "\n";
+
+          if (record) {
+            file.write(line);
+          }
         });
       });
     });
   });
 });
+
+runServer();
